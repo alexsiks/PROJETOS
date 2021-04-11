eab6352c373fb7bcbdadf9cb7aabc8c16bf865b2
Alex dos Santos Moreira <alexbilligond.una@gmail.com>
2021-04-11 10:12:09 -0300

    .
    .

--- PYTHON_SQL SEVER/src/modulos/Visualizar.py
+++
@@ -1,6 +0,0 @@
-def visualizar():
-    os.system('cls')
-    cursor = conn.cursor()
-    cursor.execute("select id,nome,sexo from USUARIO where status = 1")
-    for row in cursor:
-        print(row)
