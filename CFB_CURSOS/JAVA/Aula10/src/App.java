36fe45d6d5ae67a48fce2948e73be23ffefb37cb
Alex dos Santos Moreira <alexbilligond.una@gmail.com>
2021-04-11 18:26:29 -0300

    Aulas java
    Aula do curso do youtube do canal CFB cursos.

---
+++ CFB_CURSOS/JAVA/Aula10/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int num = 0;
+        Jogador j1 = new Jogador(num);
+
+    }
+}
---
+++ CFB_CURSOS/JAVA/Aula10/src/Jogador.java
@@ -0,0 +1,7 @@
+public class Jogador {
+
+    public Jogador(int n) {
+        System.out.printf("%n Jogador Numero %d° %n", n);
+    }
+
+}
