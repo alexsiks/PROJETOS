42e09be334854ef0a914b3c122bdd9a82c4a2266
Alex dos Santos Moreira <alexbilligond.una@gmail.com>
2021-05-21 11:59:27 -0300

    PROJETO PROTOCOLO
    neste projeto o banco de dados utilizado foi o mysql

---
+++ Protocolo/Protocolo/SistemaProtocolo.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Protocolo
+{
+    partial class SistemaProtocolo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.texto_escanear = new System.Windows.Forms.TextBox();
+            this.scanear = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "REGISTRAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.textBox1.Location = new System.Drawing.Point(12, 149);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(398, 143);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MEMORANDO: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ELOGIO",
+            "INFORMACAO",
+            "PRIORIDADE",
+            "RECLAMACAO",
+            "SOLICITACAO",
+            "SUGESTAO"});
+            this.comboBox1.Location = new System.Drawing.Point(84, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MOTIVO:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(425, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(522, 177);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(537, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "BUSCAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(425, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "SITUAÇÃO:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "ABERTO",
+            "FECHADO",
+            "EM TRATAMENTO"});
+            this.comboBox2.Location = new System.Drawing.Point(84, 49);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // texto_escanear
+            // 
+            this.texto_escanear.Location = new System.Drawing.Point(12, 104);
+            this.texto_escanear.Name = "texto_escanear";
+            this.texto_escanear.Size = new System.Drawing.Size(74, 20);
+            this.texto_escanear.TabIndex = 10;
+            // 
+            // scanear
+            // 
+            this.scanear.Location = new System.Drawing.Point(84, 104);
+            this.scanear.Name = "scanear";
+            this.scanear.Size = new System.Drawing.Size(57, 20);
+            this.scanear.TabIndex = 11;
+            this.scanear.Text = "SCAN";
+            this.scanear.UseVisualStyleBackColor = true;
+            this.scanear.Click += new System.EventHandler(this.scanear_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(244, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 20);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "ATUALIZAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // SistemaProtocolo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(959, 304);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.scanear);
+            this.Controls.Add(this.texto_escanear);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "SistemaProtocolo";
+            this.Text = "Protocolo";
+            this.Load += new System.EventHandler(this.SistemaProtocolo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox texto_escanear;
+        private System.Windows.Forms.Button scanear;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Protocolo/Protocolo/SistemaProtocolo.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+
+
+namespace Protocolo
+{
+    public partial class SistemaProtocolo : Form
+    {
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        MySqlDataAdapter da;
+        MySqlDataReader dr;
+        string strSql;
+
+        public SistemaProtocolo()
+        {
+            InitializeComponent();
+        }
+
+        
+      
+        //botão de cadastro
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text)|| string.IsNullOrWhiteSpace(comboBox1.Text))
+            {
+                MessageBox.Show("O campo não pode estar vazio.","Erro de Cadastro",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                comboBox1.Focus();
+            }
+
+            else
+            {
+
+                try
+                {
+                    conexao = new MySqlConnection("Server=192.168.100.112;Port=3306;Database=empresa;Uid=programa;Pwd=123;");
+
+                    strSql = $"insert into protocolo (situacao,motivo,memorando,dat_registro) values (@situacao,@motivo,@memorando,now())";
+
+                    comando = new MySqlCommand(strSql, conexao);
+
+                    comando.Parameters.AddWithValue("@motivo", comboBox1.Text);
+                    comando.Parameters.AddWithValue("@situacao", comboBox2.Text);
+                    comando.Parameters.AddWithValue("@memorando", textBox1.Text);
+
+                    conexao.Open();
+
+                    comando.ExecuteNonQuery();
+
+                    MessageBox.Show("Protocolo Registrado com Sucesso", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conexao.Close();
+                }
+
+
+            }
+
+        }
+        //caixa de seleção
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        //BOTÃO DE VISUALIZAÇÃO
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Não é possivel visualizar informações, informe o código do protolcolo","Erro de Busca",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+
+
+                    conexao = new MySqlConnection("Server=192.168.100.112;Port=3306;Database=empresa;Uid=programa;Pwd=123;");
+
+                    strSql = $"SELECT id as CODIGO,situacao AS SITUACAO,motivo AS MOTIVO,memorando AS DESCRICAO,dat_registro as DATA_REGISTRO,DAT_ATUALIZACAO AS DATA_ATUALIZACAO FROM PROTOCOLO where id = {textBox2.Text}";
+
+                    da = new MySqlDataAdapter(strSql, conexao);
+
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+
+
+                                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conexao.Close();
+                }
+
+            }
+
+
+        }
+
+        private void SistemaProtocolo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scanear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao = new MySqlConnection("Server=192.168.100.112;Port=3306;Database=empresa;Uid=programa;Pwd=123;");
+                strSql = "select * from protocolo where id = @ID";
+
+                comando = new MySqlCommand(strSql, conexao);
+                comando.Parameters.AddWithValue("@ID",texto_escanear.Text);
+
+                conexao.Open();
+
+                dr = comando.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    
+                    comboBox1.Text = Convert.ToString(dr["motivo"]);
+                    comboBox2.Text = Convert.ToString(dr["situacao"]);
+                    
+                    textBox1.Text = Convert.ToString(dr["memorando"]);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possivel recuperar os dados","erro",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            
+
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                conexao = new MySqlConnection("Server=192.168.100.112;Port=3306;Database=empresa;Uid=programa;Pwd=123;");
+
+                strSql = $"UPDATE protocolo SET SITUACAO = @situacao , motivo = @motivo , memorando = @memorando , DAT_ATUALIZACAO = NOW() where id = {texto_escanear.Text}";
+
+                comando = new MySqlCommand(strSql, conexao);
+
+                comando.Parameters.AddWithValue("@situacao", comboBox2.Text);
+                comando.Parameters.AddWithValue("@motivo", comboBox1.Text);
+                comando.Parameters.AddWithValue("@memorando", textBox1.Text);
+                
+                conexao.Open();
+
+                comando.ExecuteNonQuery();
+
+                MessageBox.Show("Registros atualizados", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+    }
+}
